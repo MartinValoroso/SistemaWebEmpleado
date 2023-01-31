f388030c5e6ea1bd1625620a0c9924f660661a5e
Martín Valoroso <mmvaloroso@hotmail.com>
2023-01-31 16:47:08 -0300

    Agregar archivos de proyecto.

---
+++ SistemaWebEmpleado/Models/Empleado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaWebEmpleado.Models
+{
+    public class Empleado
+
+    {
+        // Empleado (Id- Nombre-Apellido-DNI-Legajo-FechaNacimiento y Título) 
+        public int Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Apellido { get; set; }
+        public int DNI { get; set; }
+        public int Legajo { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        [Required]
+        public string Titulo { get; set; }
+
+
+    }
+}
